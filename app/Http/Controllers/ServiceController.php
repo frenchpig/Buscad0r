e49951da85d6d8ff11dc9b61d5d0ab2c8ad2fe7b
frenchpig <auspont.javier@gmail.com>
2024-03-20 09:07:35 -0300

    insertar datos en bbdd listo

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -15,10 +15,28 @@ class ServiceController extends Controller
         Log::info(gettype($data));
         // Hacer lo que necesites con los datos...
         foreach ($data as $row) {
-            Log::info('=================================================SEPARADORSEPARADORSEPARADOR=================================================');
-            Log::info($row);
-            Log::info('=================================================SEPARADORSEPARADORSEPARADOR=================================================');
-            //Servicio::create($rowData);
+            $dataLoad = [
+                'condicion' => $row['CONDICION'] ?? null,
+                'ip_loop' => $row['IP_LOOP'] ?? null,
+                'cliente' => $row['CLIENTE'] ?? null,
+                'cod' => $row['COD'] ?? null,
+                'comuna' => $row['COMUNA'] ?? null,
+                'direccion' => $row['DIRECCION'] ?? null,
+                'lan' => $row['LAN'] ?? null,
+                'protocolo' => $row['PROTOCOLO'] ?? null,
+                'servicio' => $row['SERVICIO'] ?? null,
+                'vrf' => $row['VRF'] ?? null,
+                'wan' => $row['WAN'] ?? null,
+                'nombre_equipo' => $row['NOMBRE EQUIPO'] ?? null,
+                'ip_equipo' => $row['IP EQUIPO'] ?? null,
+                'descripcion' => $row['DESCRIPCION'] ?? null,
+                'puerta' => $row['PUERTA'] ?? null,
+                'vlan' => $row['VLAN'] ?? null,
+                'posiciones' => $row['POSICIONES'] ?? null,
+                'proveedor' => $row['PROVEEDOR'] ?? null,
+                'codigo' => $row['CODIGO'] ?? null,
+            ];
+            Servicio::create($dataLoad);
         }
         // Por ejemplo, puedes devolver una respuesta indicando que los datos fueron recibidos con éxito
         return response()->json(['message' => 'Datos recibidos correctamente']);
